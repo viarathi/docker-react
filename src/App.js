d9022df0651f5bf0192b372c1873de44c0ca826d
Arathi <arathisenan5@gmail.com>
2022-06-08 23:56:30 +0530

    some feature change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was changed from the feature branch
         </a>
       </header>
     </div>
